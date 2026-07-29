5108e743bf74db4c99509f160f8c0cb4c1691253
Symbat Dulatuly <synbat1999@gmail.com>
2026-07-29 16:44:01 +0500

    Добавил анимерование дуаление и добавления

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import { motion, AnimatePresence } from "framer-motion";
 
-const PostList = ({posts, title, remove}) => {
+const PostList = ({ posts, title, remove }) => {
     if (!posts.length) {
         return (
-            <h1 style={{textAlign: "center"}}>Посты не найдены</h1>
-        )
+            <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
+        );
     }
+
     return (
         <div>
-            <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {
-                posts.map((post, index) =>
-                    <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-                )
-            }
+            <h1 style={{ textAlign: 'center' }}>{title}</h1>
+            <AnimatePresence>
+                {posts.map((post, index) => (
+                    <motion.div
+                        key={post.id}
+                        initial={{ opacity: 0, x: -350 }}  /* Появление: слева */
+                        animate={{ opacity: 1, x: 0 }}     /* Финальная позиция */
+                        exit={{ opacity: 0, x: 350 }}      /* Удаление: вправо */
+                        transition={{ duration: 0.5 }}     /* Длительность 500мс */
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     );
 };
