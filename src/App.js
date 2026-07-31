abb539408adb45680e06e7706145c002b1fa70e2
Symbat Dulatuly <synbat1999@gmail.com>
2026-07-31 21:44:18 +0500

    Пагинация

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -8,6 +8,8 @@ import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 function App() {
 
@@ -15,12 +17,22 @@ function App() {
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit));
+    })
+
+    let pagesArray = useMemo(() => getPagesArray(totalPages), [totalPages])
 
     useEffect(() => {
             fetchPosts();
         },
-        [filter]
+        [page]
     )
 
     const createPost = (newPost) => {
@@ -33,13 +45,8 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        setTimeout(async ()=> {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        }, 500);
+    const changePage = (page) => {
+        setPage(page)
     }
 
 
@@ -55,9 +62,21 @@ function App() {
 
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
+            {postError &&
+                <h1>Произошла ошибка {postError}</h1>}
             {isPostsLoading
-                ? <div style={{display:'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
-            : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Посты про JavaScript"}/>}
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Посты про JavaScript"}/>}
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span
+                        onClick = {()=>changePage(p)}
+                        className={page === p ? 'page page__current' : 'page'}
+                        key={p}>
+                        {p}
+                    </span>)}
+            </div>
+
 
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Рендерим твой компонент
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
