3b28b65eaf237bc333a07b1afc9557d80933d691
Symbat Dulatuly <synbat1999@gmail.com>
2026-08-02 19:07:21 +0500

    Пагинация отдельно

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
 import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
 
@@ -20,19 +21,19 @@ function App() {
     const [totalPages, setTotalPages] = useState(0);
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page);
         setPosts(response.data);
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPageCount(totalCount, limit));
     })
 
-    let pagesArray = useMemo(() => getPagesArray(totalPages), [totalPages])
+
 
     useEffect(() => {
-            fetchPosts();
+            fetchPosts(limit, page);
         },
-        [page]
+        [filter]
     )
 
     const createPost = (newPost) => {
@@ -46,7 +47,8 @@ function App() {
     }
 
     const changePage = (page) => {
-        setPage(page)
+        setPage(page);
+        fetchPosts(limit, page);
     }
 
 
@@ -67,17 +69,10 @@ function App() {
             {isPostsLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Посты про JavaScript"}/>}
-            <div className="page__wrapper">
-                {pagesArray.map(p =>
-                    <span
-                        onClick = {()=>changePage(p)}
-                        className={page === p ? 'page page__current' : 'page'}
-                        key={p}>
-                        {p}
-                    </span>)}
-            </div>
-
-
+            <Pagination
+                page={page}
+                changePage={changePage}
+                totalPages={totalPages}/>
         </div>
     );
 }
---
+++ src/components/UI/pagination/Pagination.jsx
@@ -0,0 +1,21 @@
+import React, {useMemo} from 'react';
+import {getPagesArray} from "../../../utils/pages";
+
+const Pagination = ({totalPages, page, changePage}) => {
+
+    let pagesArray = useMemo(() => getPagesArray(totalPages), [totalPages])
+
+    return (
+        <div className="page__wrapper">
+            {pagesArray.map(p =>
+                <span
+                    onClick = {()=>changePage(p)}
+                    className={page === p ? 'page page__current' : 'page'}
+                    key={p}>
+                        {p}
+                    </span>)}
+        </div>
+    );
+};
+
+export default Pagination;
